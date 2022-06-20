96fe8f5a4daf7530a6ce22ab6d097981c0f02227
Alvin Ng <alvinnws4546@gmail.com>
2022-06-20 15:19:00 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from './pages/layout';
+import Home from './pages/home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+			<h1> EP </h1>
+            <hr/>
+            <ul>
+                <li> Item 1 </li>
+                <li> Item 2 </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
